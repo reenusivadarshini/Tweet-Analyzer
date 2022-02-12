ed516d9da97672b500f7ea2f7f5486456d715fec
reenu123456 <98253946+reenu123456@users.noreply.github.com>
2022-02-12 23:44:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,51 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import streamlit as st
 
 
+def navigation():
+    try:
+        path = st.experimental_get_query_params()['p'][0]
+    except Exception as e:
+        st.error('Please use the main app.')
+        return None
+    return path
+
+
+if navigation() == "home":
+    st.title('Home')
+    st.write('This is the home page.')
+
+elif navigation() == "results":
+    st.title('Results List')
+    for item in range(25):
+        st.write(f'Results {item}')
+
+elif navigation() == "analysis":
+    st.title('Analysis')
+    x, y = st.number_input('Input X'), st.number_input('Input Y')
+    st.write('Result: ' + str(x+y))
+
+elif navigation() == "examples":
+    st.title('Examples Menu')
+    st.write('Select an example.')
+
+
+elif navigation() == "logs":
+    st.title('View all of the logs')
+    st.write('Here you may view all of the logs.')
+
+
+elif navigation() == "verify":
+    st.title('Data verification is started...')
+    st.write('Please stand by....')
+
+
+elif navigation() == "config":
+    st.title('Configuration of the app.')
+    st.write('Here you can configure the application')
+"""
 st.title("WELCOME!!!")
 consumer_key = "pTivamyKt0GtxZiFizMMhsouj"
 consumer_sec = "HEkXmzyaALIAD1AfTHLdRnegC8rstIY2AUHbrzvRGIjoJl3PCo"
@@ -107,5 +150,5 @@ background-size: cover;
 st.markdown(page_bg_img, unsafe_allow_html=True)
 d
 
-
+"""
 
