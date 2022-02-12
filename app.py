ccaa4bdd318854d479432a1f98c71e883949ee23
reenu123456 <98253946+reenu123456@users.noreply.github.com>
2022-02-13 02:26:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,19 +38,6 @@ api = tweepy.API(auth)
 add_selectbox = st.sidebar.selectbox("Rate the app ",("😁", "😊", "😑","😖","😡"))
 
 
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
 
 twid = st.text_input('Enter the id the person', 'your_id')
 st.write('The id is', twid)
