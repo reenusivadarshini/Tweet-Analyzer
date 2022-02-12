9abecdf6c7db0b0b6981ff6ea25ae469c0bade78
reenu123456 <98253946+reenu123456@users.noreply.github.com>
2022-02-13 01:26:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,5 +118,10 @@ hide_menu_style = """
         footer {visibility: hidden;}
         </style>
         """
+
+from PIL import Image
+image = Image.open('1.png')
+
+st.image(image, caption='Sunrise by the mountains')
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 st.markdown("[![CONTACT](1.png)](https://www.linkedin.com/in/reenu-sivadarshini-m-325429206/)")
