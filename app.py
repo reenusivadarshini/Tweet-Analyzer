2fd045949aa69cccc337abf3471e9085369c7970
reenu123456 <98253946+reenu123456@users.noreply.github.com>
2022-02-13 22:13:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ st.subheader('Scatterplot analysis')
 selected_x_var = st.selectbox('What do you want the x variable to be?', d.columns)
 selected_y_var = st.selectbox('What about the y?', d.columns)
 
-#fig = px.scatter(d, x="Analysis", y="Polarity")
+fig = px.scatter(d, x="Analysis", y="Polarity")
 fig1 = px.scatter(d, x = d[selected_x_var], y = d[selected_y_var])
 st.plotly_chart(fig1)
 
