21d3a16811612cfa4545e5cdc9506d1851cc56a7
reenu123456 <98253946+reenu123456@users.noreply.github.com>
2022-02-13 22:16:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ selected_x_var = st.selectbox('What do you want the x variable to be?', d.column
 selected_y_var = st.selectbox('What about the y?', d.columns)
 
 fig = px.scatter(d, x="Subjectivity", y="Polarity")
-fig1 = px.scatter(d, x = d[selected_x_var], y = d[selected_y_var])
+fig1 = px.scatter(d, x = d[selected_x_var], y = d[selected_y_var],color="species")
 st.plotly_chart(fig1)
 
 
